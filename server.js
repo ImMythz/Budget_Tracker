669441c731508bd0ed5a04c6fc91c0113a7fcd2a
Nate Worley <immythz@gmail.com>
2021-07-24 22:21:26 -0500

    added mongodb connection

--- server.js
+++ server.js
@@ -3,26 +3,34 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3001;;
 
 const app = express();
 
 app.use(logger("dev"));
 
+// Define middleware here
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+// Connect to the Mongo DB
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/workout',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 
 // routes
 app.use(require("./routes/api.js"));
 
+// Start the API server
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
